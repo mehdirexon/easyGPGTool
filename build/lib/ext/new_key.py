25f277c9200aef8adcc60322bfc71fb22ffd32f2
MehdiREXON <m.ghazanfari1384@gmail.com>
2023-03-09 23:43:14 +0330

    setup has been added

---
+++ build/lib/ext/new_key.py
@@ -0,0 +1,84 @@
+from PySide6.QtWidgets import QWidget,QLineEdit,QPushButton,QLabel,QComboBox,QVBoxLayout
+from PySide6.QtCore import Qt,Signal,Slot
+#-------------------------------------------------------------------------------------------------------#
+class newKeyForm(QWidget):
+#-------------------------------------------------------------------------------------------------------#
+    signal = Signal(dict)
+#-------------------------------------------------------------------------------------------------------#
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("creating a new key")
+        self.setFixedHeight(350)
+        self.setFixedWidth(300)
+        #it locks parent form when child is active
+        self.setWindowModality(Qt.ApplicationModal)
+#-------------------------------------------------------------------------------------------------------#
+        #name
+        self.fullnameLabel = QLabel("full name : ")
+        self.fullNameLineEdit= QLineEdit()
+        self.fullNameLineEdit.textEdited.connect(self.textEdited)
+
+        #email
+        self.emailLabel = QLabel("email : ")
+        self.emailLineEdit= QLineEdit()
+        self.emailLineEdit.textEdited.connect(self.textEdited)
+
+        #passphrease
+        self.passphraseLabel = QLabel("passphrase : ")
+        self.passphraseLineEdit = QLineEdit()
+        self.passphraseLineEdit.setEchoMode(QLineEdit.EchoMode.Password)
+        self.passphraseLineEdit.textEdited.connect(self.textEdited)
+
+        #the length of the key
+        self.lengthLabel = QLabel("length : ")
+        self.lengthCombo = QComboBox()
+        self.lengthCombo.addItems(["1024","2048","3072","4096"])
+
+        #the key type of the key
+        self.keyTypeLabel = QLabel("key type : ")
+        self.keyTypeCombo = QComboBox()
+        self.keyTypeCombo.addItems(["RSA"])
+
+        #create_button
+        self.createButton = QPushButton("create")
+        self.createButton.setDisabled(True)
+        self.createButton.clicked.connect(self.createClicked)
+#-------------------------------------------------------------------------------------------------------#
+        #layouts
+        V_layout1 = QVBoxLayout()
+
+        V_layout1.addWidget(self.fullnameLabel)
+        V_layout1.addWidget(self.fullNameLineEdit)
+        V_layout1.addWidget(self.emailLabel)
+        V_layout1.addWidget(self.emailLineEdit)
+        V_layout1.addWidget(self.passphraseLabel)
+        V_layout1.addWidget(self.passphraseLineEdit)
+        V_layout1.addWidget(self.lengthLabel)
+        V_layout1.addWidget(self.lengthCombo)
+        V_layout1.addWidget(self.keyTypeLabel)
+        V_layout1.addWidget(self.keyTypeCombo)
+
+        VLayout2 = QVBoxLayout()
+        VLayout2.addLayout(V_layout1)
+        VLayout2.addWidget(self.createButton,alignment=Qt.AlignCenter)
+
+        self.setLayout(VLayout2)
+#-------------------------------------------------------------------------------------------------------#
+    def textEdited(self):
+        if self.fullNameLineEdit.text() == "" or self.passphraseLineEdit.text() == "" or self.emailLineEdit == "":
+            self.createButton.setDisabled(True)
+        else:
+            self.createButton.setDisabled(False)
+#-------------------------------------------------------------------------------------------------------#
+    @Slot()
+    def createClicked(self):
+        data = {
+            "fullname" : self.fullNameLineEdit.text(),
+            "email" : self.emailLineEdit.text(),
+            "passphrase" : self.passphraseLineEdit.text(),
+            "key_length" : self.lengthCombo.currentText(),
+            "key_type" : self.keyTypeCombo.currentText()
+        }
+        self.signal.emit(data)
+        self.createButton.setDisabled(True)
+        self.close()
---
+++ build/lib/ext/trust.py
@@ -0,0 +1,89 @@
+from PySide6.QtWidgets import QWidget,QPushButton,QLabel,QVBoxLayout,QComboBox,QHBoxLayout,QDial
+from PySide6.QtCore import Qt,Signal,Slot
+#-------------------------------------------------------------------------------------------------------#
+class trustForm(QWidget):
+    signal = Signal(str)
+#-------------------------------------------------------------------------------------------------------#
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("changing a key trust level")
+        self.setFixedHeight(300)
+        self.setFixedWidth(700)
+        #it locks parent form when child is active
+        self.setWindowModality(Qt.ApplicationModal)
+#-------------------------------------------------------------------------------------------------------#
+        #fingerprints combo box
+        self.fingerprintCB = QComboBox()
+        self.fingerprintCB.currentIndexChanged.connect(self.fingerpintCBIndexChanged)
+
+        self.fingerprints = []
+        self.IDs = []
+
+        #ID Label
+        self.IDLabel = QLabel()
+
+        #trust lvl text
+        self.trustText = QLabel('TRUST_UNDEFINED')
+
+        #trust lvl
+        self.trust = ''
+
+        #change button
+        self.changeButton = QPushButton("change")
+        self.changeButton.setEnabled(False)
+        self.changeButton.clicked.connect(self.changeClicked)
+
+        #dial
+        self.trustLVL = QDial()
+        self.trustLVL.valueChanged.connect(self.trustLVLChanged)
+        self.trustLVL.setRange(1,5)
+        self.trustLVL.setNotchesVisible(True)
+#-------------------------------------------------------------------------------------------------------#
+        #layouts
+        V_layout = QVBoxLayout()
+        V_layout.addWidget(self.fingerprintCB)
+        V_layout.addWidget(self.IDLabel)
+        H_layout = QHBoxLayout()
+
+        H_layout.addWidget(self.trustLVL,alignment= Qt.AlignCenter)
+        H_layout.addWidget(self.trustText,alignment= Qt.AlignCenter)
+
+        V_layout.addLayout(H_layout)
+        V_layout.addStretch()
+        V_layout.addWidget(self.changeButton, alignment= Qt.AlignCenter)
+
+        self.setLayout(V_layout)
+#-------------------------------------------------------------------------------------------------------#
+    def trustLVLChanged(self,value):
+        if len(self.fingerprints) == 0:
+            self.changeButton.setDisabled(True)
+        else:
+            self.changeButton.setEnabled(True)
+        if value == 1:
+            self.trustText.setText('TRUST_UNDEFINED')
+            self.trust = 'TRUST_UNDEFINED'
+        elif value == 2:
+            self.trustText.setText('TRUST_NEVER')
+            self.trust = 'TRUST_NEVER'
+        elif value == 3:
+            self.trustText.setText('TRUST_MARGINAL')
+            self.trust = 'TRUST_MARGINAL'
+        elif value == 4:
+            self.trustText.setText('TRUST_FULLY')
+            self.trust = 'TRUST_FULLY'
+        else:
+            self.trustText.setText('TRUST_ULTIMATE')
+            self.trust = 'TRUST_ULTIMATE'
+#-------------------------------------------------------------------------------------------------------#
+    def fingerpintCBIndexChanged(self,index):
+        self.IDLabel.setText(self.IDs[index][0])
+    def getFingerprints(self,fp):
+        for key in fp:
+            self.fingerprints.append(key['fingerprint'])
+            self.IDs.append(key['uids'])
+        self.fingerprintCB.addItems(self.fingerprints)
+#-------------------------------------------------------------------------------------------------------#
+    @Slot()
+    def changeClicked(self):
+        self.signal.emit(self.trust)
+        self.close()
