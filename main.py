59afd9372f79b18c2a141260552e14879802aeaf
MehdiREXON <m.ghazanfari1384@gmail.com>
2023-03-10 23:15:05 +0330

    version 0.2 changes

--- main.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication
-from ext.mainWindow import mainWindow
-import sys
-
-app = QApplication(sys.argv)
-window = mainWindow(app,"GPG tool")
-window.show()
-
-app.exec()
--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
-from setuptools import setup
-  
+from setuptools import setup,find_packages
+with open('README.md','r')as f:
+    long_description = f.read()
 setup(
-    name='GPG Tool',
-    version='beta',
-    description='A GUI GPG tool',
-    author='Mehdi GHazanfari',
-    author_email='Mehdi Ghazanfari',
-    packages=['ext'],
+    name='easyGPG',
+    packages= find_packages(),
+    version='0.2',
+    description='A GUI GPG tool application',
+    long_description = long_description,
+    author='Mehdi Ghazanfari',
+    author_email='mehdirexon@gmail.com',
+    url = 'https://github.com/MehdiREXON/easyGPG',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.1",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Security :: Cryptography"
+    ],
     install_requires=[
         'plyer',
         'colorama',
         'python-gnupg',
         'python-magic',
         'python-dateutil',
-        'PySide6-Essentials',
-        'PySide6-Addons',
-        'PySide6',
+        'PySide6'
     ],
+    python_requires = ">=3.10"
 )
