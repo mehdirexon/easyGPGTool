eee250d58c4d16fa296c58de9b2b73c3d4cce9c4
MehdiREXON <m.ghazanfari1384@gmail.com>
2023-06-13 17:05:42 +0330

    form has been updated
    user is able to copy the passoword by clicking on it,whenever the passowrd has been copied it shows a notifacation

--- easyGPGTool/_passGen_.py
+++ easyGPGTool/_passGen_.py
@@ -1,48 +1,77 @@
-from PySide6.QtWidgets import QWidget,QLineEdit,QPushButton,QLabel,QVBoxLayout,QApplication,QComboBox
+from PySide6.QtWidgets import QWidget,QLineEdit,QPushButton,QLabel,QVBoxLayout,QApplication,QSlider,QCheckBox
 from PySide6.QtCore import Qt,Signal,Slot
-from PySide6.QtGui import QScreen
-#-------------------------------------------------------------------------------------------------------#
+from PySide6.QtGui import QScreen,QClipboard
+from passlib.hash import pbkdf2_sha256
+from passlib import pwd
+from plyer import notification
 class passGenForm(QWidget):
     signal = Signal(str)
-#-------------------------------------------------------------------------------------------------------#
+
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("password generator")
-        self.setFixedHeight(120)
-        self.setFixedWidth(300)
+        self.setWindowTitle("Password Generator")
+
+        self.setFixedHeight(180)
+        self.setFixedWidth(500)
         #it locks parent form when child is active
         self.setWindowModality(Qt.ApplicationModal)
-#-------------------------------------------------------------------------------------------------------#
-        #level of difficulty
-        self.paswrodLVLLabel = QLabel("select your password difficulty : ")
-        self.passwordLVL = QComboBox()
-        self.passwordLVL.addItems(["weak","fair","strong","secure"])
-        self.passwordLVL.setCurrentIndex(2)
 
-        #decrypt_button
-        self.generateButton = QPushButton("generate")
-        #self.decryptButton.setDisabled(True)
-        self.generateButton.clicked.connect(self.generateClicked)
-#-------------------------------------------------------------------------------------------------------#
-        #layouts
+        self.paswrodLVLLabel = QLabel("Select your password difficulty : ")
+
+        self.difficultySlider = QSlider(Qt.Horizontal,self)
+        self.difficultySlider.setToolTip("Adjust the difficulty level to make the generated password harder to guess or crack. Higher difficulty levels result in stronger passwords.")
+        self.difficultySlider.valueChanged.connect(self.sliderValueChanged)
+        self.difficultySlider.setRange(50,300)
+
+        self.informationLabel = QLabel("By clicking on the password box below, you can copy it")
+
+        self.passLineEdit = QLineEdit(self)
+        self.passLineEdit.mousePressEvent = self.passLineEditClicked
+        self.passLineEdit.setReadOnly(True)
+
+        self.saveCheckBox = QCheckBox("Save")
+        self.saveCheckBox.setToolTip("Check this box to save the generated password in the app’s database.")
+        self.closeButton = QPushButton("Close")
+        self.closeButton.clicked.connect(self.closeClicked)
+
         V_layout = QVBoxLayout()
-
         V_layout.addWidget(self.paswrodLVLLabel)
-        V_layout.addWidget(self.passwordLVL)
-
+        V_layout.addWidget(self.difficultySlider)
+        V_layout.addWidget(self.informationLabel)
+        V_layout.addWidget(self.saveCheckBox)
+        V_layout.addWidget(self.passLineEdit)
         V_layout.addStretch()
-        V_layout.addWidget(self.generateButton, alignment= Qt.AlignCenter)
+        V_layout.addWidget(self.closeButton, alignment= Qt.AlignCenter)
 
         self.setLayout(V_layout)
-#-------------------------------------------------------------------------------------------------------#
+
+    def sliderValueChanged(self, value):
+        password, hashed = self.generatePassword(value)
+        self.passLineEdit.setText(str(password))
+
     def showEvent(self, event):
         super().showEvent(event)
         center = QScreen.availableGeometry(QApplication.primaryScreen()).center()
         geo = self.frameGeometry()
         geo.moveCenter(center)
         self.move(geo.topLeft())
-#-------------------------------------------------------------------------------------------------------#
+
+    def passLineEditClicked(self,event):
+        clipboard = QClipboard()
+        clipboard.setText(str(self.passLineEdit.text()))
+        notification.notify(
+            title = 'easy GPG tool notification',
+            message = 'the password has been copied successfully',
+            app_name='easy GPG tool'
+        )
+
+
     @Slot()
-    def generateClicked(self):
-        self.signal.emit(self.passwordLVL.currentText())   
+    def closeClicked(self):
+        #self.signal.emit(self.passwordLVL.currentText())   
         self.close()
+
+    def generatePassword(self,entropy:int):
+        password : str = pwd.genword(entropy)
+        hashed_password : str = pbkdf2_sha256.hash(password)
+        return (password, hashed_password)
