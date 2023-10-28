f107cb770c2a6aec80d9ca720f776e6623f5f741
Rexon <rexon@rexon-pc.local>
2023-10-28 12:24:08 +0330

    aes encryption is added

--- easyGPGTool/Algorithm/aes.py
+++ easyGPGTool/Algorithm/aes.py
@@ -1,2 +1,27 @@
 from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from typing import Union
 
+
+class easyAES:
+    @staticmethod
+    def encrypt(plainText: Union[str, bytes], key) -> Union[str, bytes]:
+        iv = get_random_bytes(16)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        plainText = plainText + b" " * (16 - len(plainText) % 16)
+        ciphertext = cipher.encrypt(plainText)
+
+        return iv + ciphertext
+
+    @staticmethod
+    def decrypt(ciphertext, key):
+        iv = ciphertext[:16]
+        ciphertext = ciphertext[16:]
+
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+
+        plaintext = cipher.decrypt(ciphertext)
+
+        plaintext = plaintext.rstrip(b" ")
+
+        return plaintext
